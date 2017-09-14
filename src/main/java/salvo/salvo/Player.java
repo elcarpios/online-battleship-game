6dd3d89e9b37784a3e4503d2324c20d234576304
vCarpio <elcarpiosbit@gmail.com>
2017-09-14 12:43:18 +0200

    CREATED a Fake Repository of Players

--- src/main/java/salvo/salvo/Player.java
+++ src/main/java/salvo/salvo/Player.java
@@ -5,34 +5,40 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity // Tells Spring to create a salvo.salvo.Player table for this class
+
+// @SomeWord order, is only affecting at the next line of code
+@Entity // Tells Spring to create a Player table for this class
 public class Player {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Id //  Says that the id instance variable holds the database key for this class
+    @GeneratedValue(strategy=GenerationType.AUTO) // Tells JPA to get the Id from the DBMS.
     private long id;
 
+    private String name;
     private String userName;
+    private String password;
 
-    public Player() { }
+    public Player() { } // We need an empty constructor for the internal uses
 
-    public Player(String userName) {
+    public Player(String name, String userName, String password) {
+        this.name = name;
         this.userName = userName;
+        this.password = password;
     }
 
     public long getId() {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public String getName() {
+        return name;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public String getPassword() {
+        return password;
     }
 }
--- src/main/java/salvo/salvo/SalvoApplication.java
+++ src/main/java/salvo/salvo/SalvoApplication.java
@@ -1,12 +1,24 @@
 package salvo.salvo;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Tells Spring to look for cases in the code that need instances of bean
 public class SalvoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean // Mark a method that returns an instance of a Java bean
+	public CommandLineRunner initData(PlayerRepository repository) {
+		return (args) -> {
+			repository.save(new Player("Jack Bauer", "j.bauer@ctu.gov", "24"));
+			repository.save(new Player("Chloe O'Brian", "c.obrian@ctu.gov", "42"));
+			repository.save(new Player("Kim Bauer", "kim_bauer@gmail.com", "kb"));
+			repository.save(new Player("Tony Almeida", "t.almeida@ctu.gov", "mole"));
+		};
 	}
 }
