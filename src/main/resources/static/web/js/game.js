ad68ad9c6d2237d12558f20d97cc2f3cee19e069
vCarpio <elcarpiosbit@gmail.com>
2017-10-25 13:37:32 +0200

    game.html style changed. Adding method to put the ships on the grid

--- src/main/resources/static/web/js/game.js
+++ src/main/resources/static/web/js/game.js
@@ -1,14 +1,48 @@
 $(document).ready(function() {
 	
 	let urlParameters = paramObj(window.location.href);
-	//let urlJson = 'gamedata.json';
-	let urlJson =  ' /api/game_view/' + urlParameters.gp;
+	//let urlJson = 'gamedummy.json';
+	let urlJson = 'gamedata.json';
+	//let urlJson =  ' /api/game_view/' + urlParameters.gp;
 	
 	getJsonAndStartFunctions(urlJson);	
 	
 	setLogoutForm();
 	
+	let ships = true;
+	
+	if(ships) {
+		setShips();
+	}
+	
 });
+
+
+// Set functions to colocate ships on grid 
+function setShips() {
+	
+	let ships = document.getElementsByClassName('ship');
+	console.log(ships);
+	
+	for(ship of ships) {
+		ship.addEventListener('click',function(){setShip(this.id);});
+	}
+}
+
+function setShip(type) {
+	
+	// First hide the set ship layers
+	let layers = document.getElementsByClassName('ships-layer');
+	console.log(layers);
+	for(layer of layers) {
+		console.log(layer);
+		layer.style.visibility = 'hidden';
+	}
+	
+	// Let to put the ships on grid
+	
+	
+}
 
 
 function setLogoutForm() {
@@ -47,31 +81,103 @@ function getJsonAndStartFunctions(url) {
 }
 
 function startFunctions(data) {
-
-	// Define the prefixes for duplicate values, like coordinades in grid
-	let ownPrefix = 'own-';
-	let ownTurnsSalvoes = data.salvoes[1];
 	
-	let opositePrefix = 'op-';
-	let opositeTurnsSalvoes = data.salvoes[0];
-
-	infoPlayerConstructor(data.gamePlayers);
 	console.log(data);
+
+	// Construct players panel
+	infoPlayerConstructor(data.gamePlayers);
 	
-	var ownGrid = gridConstructor('ownGrid',10,10,ownPrefix);
-	document.body.appendChild(ownGrid);
-	setShipsOnGrid(ownPrefix, data.ships);
-	setSalvoesOnGrid(ownPrefix, ownTurnsSalvoes);
 	
-	var oppositeGrid = gridConstructor('oppositeGrid',10,10,opositePrefix);
-	document.body.appendChild(oppositeGrid);
-	setSalvoesOnGrid(opositePrefix, opositeTurnsSalvoes);
+	// Own
+		let ownPrefix = 'own';
+		let ownTurnsSalvoes = (data.salvoes != null) ? data.salvoes[1] : 0;
+	
+		let ownTitle = document.createElement('h1');
+		ownTitle.className = 'title-board';
+		ownTitle.innerHTML = 'Your Board';
+		document.getElementById('own-board').appendChild(ownTitle);
+
+		let ownGrid = gridConstructor('ownGrid',ownPrefix);
+		document.getElementById('own-board').appendChild(ownGrid);
+		
+		let ownShips = shipsContainerConstructor('own');
+		document.getElementById('own-board').appendChild(ownShips);
+
+		// Exists ships to print?
+		if(data.ships != null) 
+			putShipsOnGrid(ownPrefix, data.ships);
+
+		// Exists salvoes to print?
+		if(ownTurnsSalvoes.length > 0) 
+			putSalvoesOnGrid(ownPrefix, ownTurnsSalvoes);
+	
+	
+	// Opposite	
+		let opositePrefix = 'op';
+		let opositeTurnsSalvoes = (data.salvoes != null) ? data.salvoes[0] : 0;
+	
+		let opTitle = document.createElement('h1');
+		opTitle.className = 'title-board';
+		opTitle.innerHTML = 'Enemy Board';
+		document.getElementById('op-board').appendChild(opTitle);
+
+		let oppositeGrid = gridConstructor('oppositeGrid',opositePrefix);
+		document.getElementById('op-board').appendChild(oppositeGrid);
+	
+		let opShips = shipsContainerConstructor('op');
+		document.getElementById('op-board').appendChild(opShips);
+
+		// Exists salvoes to print?
+		if(opositeTurnsSalvoes.length > 0) 
+				putSalvoesOnGrid(opositePrefix, opositeTurnsSalvoes);
+	
+
 }
+
+
+// Create the legend to contain the ships for each board
+function shipsContainerConstructor(who) {
+	
+	let container = document.createElement('div');
+	
+	let shipsList = ['Destroyer','Submarine','Patrol Boat'];
+	
+	// Add the ships into the box
+	let legend = addShipsToLegend(who,shipsList);
+	
+	container.appendChild(legend);
+	
+	return container;
+	
+}
+
+
+function addShipsToLegend(who,listShips) {
+	
+	let ships = document.createElement('div');
+	
+	for(let i=0; i<listShips.length; i++) {
+		
+		let ship = document.createElement('div');
+		if(who == 'own') {
+			ship.id = listShips[i];
+			ship.setAttribute('draggable','true');
+			ship.setAttribute('ondragstart','drag(event)');
+		}
+		ship.innerHTML = listShips[i];
+		ships.appendChild(ship);
+		
+	}
+	
+	return ships;
+	
+}
+
 
 function infoPlayerConstructor(players) {
 	let urlParameters = paramObj(window.location.href);
-	var id = urlParameters.gp;
-	//var id = 1;
+	//var id = urlParameters.gp;
+	var id = 1;
 	let infoPlayer = document.createElement('div');
 	
 	for(let i=0; i<players.length; i++) {
@@ -84,64 +190,68 @@ function infoPlayerConstructor(players) {
 	}
 
 	
-	document.body.appendChild(infoPlayer);
+	document.getElementById('players-info').appendChild(infoPlayer);
 }
 
-function gridConstructor(idDiv,col,row,cellIdPrefix) {
+function gridConstructor(idDiv,idPrefix) {
+	
 	// Create the grid
-	let grid = document.createElement('div');
+	let grid = document.createElement('table');
 	grid.id = idDiv;
 	grid.className = 'grid';
 	
 	
 	// Initialize all the indicators (references in grid)
-	var letterIndicators = ["","A","B","C","D","E","F","G","H","I","J"];
-	var numberIndicators;
-	var colIdIndicator = 1;
-	var rowIdIndicator = 1;
+	var header = ["","A","B","C","D","E","F","G","H","I","J"];
+	
 	
 	// Add 1 to row and col to include headers
-	for(let i=0; i<((col+1)*(row+1)); i++) {
-
-		let gridItem = document.createElement('div');
-		gridItem.className = 'grid-item';
+	for(let i=0; i<=10; i++) {
 		
-		// Check if we are doing the first row to put the indicators
-		if( i <= col ) {
-			gridItem.className += ' grid-header';
-			gridItem.innerHTML = letterIndicators[i];
-		}
+		var row = document.createElement('tr');
 		
-		// Check if we are doing the first column to put the indicators
-		if ( i!=0 && i % 11 == 0) {
-			gridItem.className += ' grid-header';
-			numberIndicators = i.toString(); 
-			gridItem.innerHTML = numberIndicators.slice(1);
-		}
-		
-		// If is not a header we add an ID
-		if(!gridItem.classList.contains('grid-header')) {
-			gridItem.id = cellIdPrefix + letterIndicators[colIdIndicator] + rowIdIndicator.toString();
+		for(let j=0; j<=10; j++) {
 			
-			// Calculate the new indicator
-			colIdIndicator += 1;
-			if(colIdIndicator % 11 == 0) {
-				colIdIndicator = 1;
-				rowIdIndicator++;
+			// Create standard cell
+			let cell = document.createElement('td');
+			cell.className = 'grid-item';
+
+			// Column header
+			if( i == 0 ) {
+				cell.className += ' col-header';
+				cell.innerHTML = header[j];
+			} 
+			// Row header
+			else if( (j == 0) && (i > 0) ) {
+				cell.className += ' row-header';
+				cell.innerHTML = i;
 			}
+			// Normal cells
+			else {
+				cell.id = idPrefix + header[i] + j;
+				cell.className += ' cell';
+				cell.setAttribute('ondrop','drop(event)');
+				cell.setAttribute('ondragover','allowDrop(event)');
+			}
+			
+			row.appendChild(cell);
+			
 		}
 		
-		grid.appendChild(gridItem);
+		grid.appendChild(row);
+
 	}
 	
 	return grid;
 }
 
 
-function setShipsOnGrid(prefix,ships) {
+function putShipsOnGrid(prefix,ships) {
 
 	for(let i=0; i<ships.length; i++) {
+		
 		let ship = ships[i];
+		
 		switch (ship.Type) {
 			
 		case 'Destroyer': 	implementClasses(prefix,' destroyer',ship.Locations);	
@@ -158,29 +268,122 @@ function setShipsOnGrid(prefix,ships) {
 }
 
 function implementClasses(prefix,classes, array) {
+	
 	for(let j=0; j<array.length; j++) {
-	let gridItem = document.getElementById(prefix + array[j]);
-	gridItem.className += ' ' + classes;
+		
+		let gridItem = document.getElementById(prefix + array[j]);
+		gridItem.className += ' ' + classes;
+		
 	} 
+	
 }
 
-function setSalvoesOnGrid(prefix, turns) {
+function putSalvoesOnGrid(prefix, turns) {
+	
 	// Go throw all the turns
 	for (var i=0; i<turns.length; i++) {
+		
 		let salvoes = turns[i];
+		
 		for (let j=0; j<salvoes.length; j++) {
+			
 			let salvo = salvoes[j];
+			
 			let salvoCell = document.getElementById(prefix + salvo);
 			let classesCell = salvoCell.classList;
+			
 			if((classesCell.value.indexOf('destroyer') != -1) ||
 				(classesCell.value.indexOf('submarine') != -1) ||
 				(classesCell.value.indexOf('patrol-boat') != -1)) {
+				
 				salvoCell.className += ' bombed';
+				
 			}
 			else {
+				
 				salvoCell.className += ' water';
+				
 			}
+			
 			salvoCell.innerHTML = i + 1;
+			
 		}
 	}
+}
+
+
+
+// DRAG CODE
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+	
+    ev.preventDefault();
+	
+    var data = ev.dataTransfer.getData("text");
+		
+	
+		// Get the prefix of the row
+		let prefix = ev.target.id.slice(0,3);
+		// Get the letter of the row
+		let letter = ev.target.id.slice(3,4);
+		// Get the number of the col
+		let position = parseInt(ev.target.id.slice(4));
+	
+	
+		// Set the ship size
+		let length = (data == 'Patrol Boat') ? 2 : 3;
+	
+		// Check if fits in the position
+		if(isFit(position,length)) {
+			
+			// Create locations for the ship
+			let locations = createLocations(letter,position,length);
+			
+			// Create the object ship to send into printer
+			let ship = [{
+				Type: data,
+				Locations: locations
+			}];
+			
+			putShipsOnGrid(prefix,ship);
+			
+			//Delete dragable properties of the ship
+			// Delete the ship in the legend
+			let legend = document.getElementById(data);
+			legend.parentNode.removeChild(legend);
+			
+		}	
+			
+		 else {
+			alert('No real position');
+		}
+}
+
+
+// Create locations array
+function createLocations(letter,position,length) {
+	
+	let locations = [];
+	
+	for(let i=position; i<position+length; i++ ) {
+		
+		locations.push(letter + i);
+		
+	}
+	
+	return locations;
+}
+
+
+// Check if the ship fits into the grid
+function isFit(pos,length) {
+	return pos + length <= 11;
 }
